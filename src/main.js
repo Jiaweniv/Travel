fc489fd36dbb98bf55a0b044b26a022c4cb00c4a
admin <1154309574@qq.com>
2018-07-19 10:06:33 +0800

    up

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/normalize.css'//初始化样式
+import './assets/styles/border.css' //1像素高倍屏结局方案
+//click 事件延迟300s 解决方案  npm install fastclick --save
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
