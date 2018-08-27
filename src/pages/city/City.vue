3f8108342dc890e3887591f7f696bfa6a79101b8
admin <1154309574@qq.com>
2018-08-27 16:43:03 +0800

    city up

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 <script>
@@ -11,6 +11,7 @@
     import CitySearch from './components/Search.vue'
     import CityList from './components/List.vue'
     import CityAlphabet from './components/Alphabet.vue'
+    import axios from 'axios'
     export default{
         name: 'City',
         components: {
@@ -18,6 +19,29 @@
             CitySearch,
             CityList,
             CityAlphabet
+        },
+        data () {
+            return{
+                cities: {},
+                hotCities: []
+            }
+        },
+        mounted(){
+            this.getCityInfo()
+        },
+        methods: {
+            getCityInfo(){
+                axios.get('/api/city.json')
+                    .then(this.getCityInfoSucc)
+            },
+            getCityInfoSucc(res){
+                res = res.data;
+                if(res.ret && res.data){
+                    const data = res.data
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                }
+            }
         }
     }
 </script>
