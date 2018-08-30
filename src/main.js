0d2fd65f6dc5a7c8cb8dd20e6688e08cc55597e3
lzh <zarva.lee@gmail.com>
2018-08-30 16:55:22 +0800

    up

--- src/main.js
+++ src/main.js
@@ -6,11 +6,12 @@ import router from './router'
 import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
-
+import 'babel-polyfill'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+
 
 
 Vue.config.productionTip = false
